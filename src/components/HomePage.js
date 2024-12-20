ac1e29eae8652283e9825445fe70da6b142a8b19
Andrew Chow <ac.andrewchow@gmail.com>
2024-12-19 23:34:19 -0500

    Updates home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,21 +3,17 @@ import TableOfContents from "./TableOfContents";
 
 const HomePage = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen bg-zinc-900 text-white">
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-zinc-900 text-white p-8">
       <TableOfContents />
-      <div className="max-w-4xl text-center p-8 bg-white/10 backdrop-blur-md rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold mb-6">Intermediate Programming</h1>
-        <p className="text-lg mb-6">
-          Welcome to the CIS2500 - Intermediate Programming
-        </p>
-        <div className="flex justify-center">
-          <img
-            src="/images/guelph.jpg"
-            alt="Abstract Programming Art"
-            className="w-2/3 rounded-md shadow-md"
-          />
-        </div>
-      </div>
+      <h1 className="text-4xl font-bold mb-6">Intermediate Programming</h1>
+      <p className="text-lg mb-6">Welcome to CIS2500</p>
+
+      <a
+        href="/installation"
+        className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 inline-block"
+      >
+        Installation Instructions
+      </a>
     </div>
   );
 };
