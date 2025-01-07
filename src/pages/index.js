a3f9d3385e91dcdfbe0745d04c23d7dbe622f7b1
Andrew Chow <ac.andrewchow@gmail.com>
2025-01-06 23:28:02 -0500

    Adds missing image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 "use client";
-import CodeRunner from "@/components/CodeRunner";
 import HomePage from "@/components/HomePage";
 import Footer from "@/components/Footer";
 
@@ -7,10 +6,6 @@ export default function Home() {
   return (
     <div className="bg-zinc-900">
       <HomePage />
-      {/* <CodeRunner
-        codeFilePath="/code-examples/helloWorld.c" 
-        apiEndpoint="/api/compile" 
-      /> */}
       <Footer />
     </div>
   );
