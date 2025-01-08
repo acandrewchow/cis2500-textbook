2c97b1594cc2206a9208f634c343523fae3726fb
Andrew Chow <ac.andrewchow@gmail.com>
2025-01-08 18:54:38 -0500

    Updates home page for quizzes

--- src/pages/quizzes/home.js
+++ src/pages/quizzes/home.js
@@ -4,18 +4,57 @@ import TableOfContents from "@/components/TableOfContents";
 
 const Home = () => {
   const quizzes = [
-    { label: "Quiz 1: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizOne" },
-    { label: "Quiz 2: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizTwo" },
-    { label: "Quiz 3: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizThree" },
-    { label: "Quiz 4: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizFour" },
-    { label: "Quiz 5: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizFive" },
-    { label: "Quiz 6: Lorem Ipsum", description: "Lorem Ipsum", link: "/quizzes/quizSix" },
+    {
+      label: "Quiz 1: Review & Compilation",
+      description: "",
+      link: "/quizzes/quizOne",
+    },
+    {
+      label: "Quiz 2: Binary Files",
+      description: "",
+      link: "/quizzes/quizTwo",
+    },
+    {
+      label: "Quiz 3: Memory",
+      description: "",
+      link: "/quizzes/quizThree",
+    },
+    {
+      label: "Quiz 4: Recursion",
+      description: "",
+      link: "/quizzes/quizFour",
+    },
+    {
+      label: "Quiz 5: Linked Lists",
+      description: "",
+      link: "/quizzes/quizFive",
+    },
+    {
+      label: "Quiz 6: Stacks and Queues",
+      description: "",
+      link: "/quizzes/quizSix",
+    },
+    {
+      label: "Quiz 7: Function Pointers",
+      description: "",
+      link: "/quizzes/quizSeven",
+    },
+    {
+      label: "Quiz 8: Algorithms - Searching & Sorting",
+      description: "",
+      link: "/quizzes/quizEight",
+    },
+    {
+      label: "Quiz 9: Review",
+      description: "",
+      link: "/quizzes/quizNine",
+    },
   ];
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-900 text-white p-8">
       <TableOfContents />
-      <h1 className="text-4xl font-bold mt-12 mb-6 text-center">Quizzes</h1>      
+      <h1 className="text-4xl font-bold mt-12 mb-6 text-center">Quizzes</h1>
       <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3 w-full max-w-4xl">
         {quizzes.map((quiz, index) => (
           <div
@@ -23,7 +62,6 @@ const Home = () => {
             className="bg-zinc-800 rounded-lg shadow-xl hover:shadow-2xl transform hover:scale-105 transition-all duration-300 p-6 flex flex-col justify-between"
           >
             <h3 className="text-xl font-semibold mb-2">{quiz.label}</h3>
-            <p className="text-gray-400 mb-6">{quiz.description}</p>
             <Link
               href={quiz.link}
               className="text-blue-500 hover:underline text-sm absolute bottom-4 right-4"
