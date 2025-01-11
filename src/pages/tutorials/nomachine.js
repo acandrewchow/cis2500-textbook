df342b958066a3dc1bda7877268b7e5554d6ab2e
Andrew Chow <ac.andrewchow@gmail.com>
2025-01-10 20:27:37 -0500

    Migrates noMachineContent to json

--- src/pages/tutorials/nomachine.js
+++ src/pages/tutorials/nomachine.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TableOfContents from "@/components/TableOfContents";
+import noMachineContent from "@/data/tutorials/noMachineContent.json"; 
 
 const NoMachineTutorial = () => {
   return (
@@ -7,63 +8,54 @@ const NoMachineTutorial = () => {
       <TableOfContents />
       <div className="bg-zinc-800 mb-24 shadow-md rounded-lg p-8 max-w-3xl w-full">
         <h1 className="text-4xl font-bold mb-4 text-center text-white">
-          NoMachine Tutorial
+          {noMachineContent.title}
         </h1>
-        <p className="text-lg mb-6 text-center">
-          Welcome to the NoMachine tutorial. Follow the steps below to get
-          started.
-        </p>
+        <p className="text-lg mb-6 text-center">{noMachineContent.intro}</p>
         <div className="space-y-4">
-          <div className="p-4 bg-zinc-700 border-l-4 border-blue-600 rounded">
-            <h2 className="text-xl font-semibold">
-              Step 1: Download NoMachine
-            </h2>
-            <p className="text-white">
-              NoMachine is available for both Windows and MacOS, please visit{" "}
-              <a
-                href="https://wiki.socs.uoguelph.ca/techsupport/guides/nomachine"
-                target="_blank"
-                rel="noreferrer noopener"
-                className="text-blue-500 hover:underline"
-              >
-                Wiki Socs
-              </a>{" "}
-              to download the software.
-            </p>
-          </div>
-          <div className="p-4 bg-zinc-700 border-l-4 border-blue-600 rounded">
-            <h2 className="text-xl font-semibold">
-              Step 2: Setup Configurations
-            </h2>
-            <p className="text-white">
-              Follow the setup instructions provided on the website to set up
-              NoMachine on your local machine.
-            </p>
-          </div>
-          <div className="p-4 bg-zinc-700 border-l-4 border-blue-600 rounded">
-            <h2 className="text-xl font-semibold">
-              Step 3: Connect to the School's Linux Servers
-            </h2>
-            <p className="text-white">
-              Open NoMachine, enter the IP address of the linux servers{" "}
-              <code className="font-bold bg-gray-800 rounded-md">
-                nomachine.linux.socs.uoguelph.ca
-              </code>
-              , and follow the prompts to connect to the school's linux servers.
-            </p>
-          </div>
+          {noMachineContent.steps.map((step, index) => (
+            <div
+              key={index}
+              className="p-4 bg-zinc-700 border-l-4 border-blue-600 rounded"
+            >
+              <h2 className="text-xl font-semibold">{step.title}</h2>
+              <p className="text-white">{step.content}</p>
+              {step.link && (
+                <p>
+                  Visit{" "}
+                  <a
+                    href={step.link.url}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="text-blue-500 hover:underline"
+                  >
+                    {step.link.text}
+                  </a>
+                </p>
+              )}
+              {step.code && (
+                <p>
+                  <strong>Server IP Address:</strong>{" "}
+                  <code className="font-bold bg-gray-800 rounded-md">
+                    {step.code}
+                  </code>
+                </p>
+              )}
+            </div>
+          ))}
 
           <div className="p-4 bg-zinc-700 border-l-4 border-blue-600 rounded">
-            <h2 className="text-xl font-semibold mb-4">Demo</h2>
-            <p className="text-white"></p>
+            <h2 className="text-xl font-semibold mb-4">{noMachineContent.demo.title}</h2>
             <div className="mb-6">
               <video
-                width="100%"
-                height="auto"
+                width={noMachineContent.demo.video.width}
+                height={noMachineContent.demo.video.height}
                 controls
                 className="rounded-lg shadow-lg"
               >
-                <source src="/demos/noMachine.mov" type="video/mp4" />
+                <source
+                  src={noMachineContent.demo.video.src}
+                  type={noMachineContent.demo.video.type}
+                />
               </video>
             </div>
           </div>
