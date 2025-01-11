3b3ef7d663b3668014eb0b868223051cd3bb7740
Andrew Chow <ac.andrewchow@gmail.com>
2025-01-11 11:55:03 -0500

    Adds exerciseRoutes

--- src/pages/exercises/review/topStudent.js
+++ src/pages/exercises/review/topStudent.js
@@ -7,7 +7,7 @@ const topStudentExercise = () => {
     <div className="min-h-screen bg-zinc-900 text-white flex flex-col items-center">
       <TableOfContents />
       <div className="w-full max-w-5xl mt-12">
-        <CodingExercise jsonFilePath="/code-examples/exercises/review/structExercise.json" />
+        <CodingExercise jsonFilePath="/code-examples/exercises/review/topStudent.json" />
       </div>
     </div>
   );
